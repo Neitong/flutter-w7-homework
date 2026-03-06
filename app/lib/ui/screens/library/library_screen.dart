e10b0eb08055502fb3a6e3af26751823b8fc4f7d
Neitong <Hengneitong.keo@student.cadt.edu.kh>
2026-03-06 09:11:55 +0700

    small update

--- app/lib/ui/screens/library/library_screen.dart
+++ app/lib/ui/screens/library/library_screen.dart
@@ -1,91 +1,28 @@
+import 'package:app/data/repositories/songs/song_repository.dart';
+import 'package:app/ui/screens/library/library_content.dart';
+import 'package:app/ui/screens/library/model/library_view_model.dart';
+import 'package:app/ui/states/player_state.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
- 
-import '../../../data/repositories/songs/song_repository.dart';
-import '../../../model/songs/song.dart';
-import '../../states/player_state.dart';
-import '../../states/settings_state.dart';
-import '../../theme/theme.dart';
 
 class LibraryScreen extends StatelessWidget {
   const LibraryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // 1- Read the globbal song repository
-    SongRepository songRepository = context.read<SongRepository>();
-    List<Song> songs = songRepository.fetchSongs();
+    final songRepository = context.read<SongRepository>();
+    final playerState = context.read<PlayerState>();
 
-    AppSettingsState settingsState = context.watch<AppSettingsState>();
-
-    // 3 - Watch the globbal player state
-    PlayerState playerState = context.watch<PlayerState>();
-
-    return Container(
-      color: settingsState.theme.backgroundColor,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 16),
-          Text("Library", style: AppTextStyles.heading),
-
-          SizedBox(height: 50),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: songs.length,
-              itemBuilder: (context, index) => SongTile(
-                song: songs[index],
-                isPlaying: playerState.currentSong == songs[index],
-                isFavorite: playerState.isFavorite(songs[index]),
-                onTap: () {
-                  playerState.start(songs[index]);
-                },
-                onFavoriteTap: () {
-                  playerState.toggleFavorite(songs[index]);
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SongTile extends StatelessWidget {
-  const SongTile({
-    super.key,
-    required this.song,
-    required this.isPlaying,
-    required this.isFavorite,
-    required this.onTap,
-    required this.onFavoriteTap,
-  });
-
-  final Song song;
-  final bool isPlaying;
-  final bool isFavorite;
-  final VoidCallback onTap;
-  final VoidCallback onFavoriteTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-      title: Text(song.title),
-      subtitle: Text(song.artist),
-      leading: IconButton(
-        onPressed: onFavoriteTap,
-        icon: Icon(
-          isFavorite ? Icons.favorite : Icons.favorite_border,
-          color: isFavorite ? Colors.red : null,
-        ),
-      ),
-      trailing: Text(
-        isPlaying ? "Playing" : "",
-        style: TextStyle(color: Colors.amber),
-      ),
+    return ChangeNotifierProvider<LibraryViewModel>(
+      create: (_) {
+        final viewModel = LibraryViewModel(
+          songRepository: songRepository,
+          playerState: playerState,
+        );
+        viewModel.init();
+        return viewModel;
+      },
+      child: const LibraryContent(),
     );
   }
 }
